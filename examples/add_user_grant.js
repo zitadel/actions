6fec059dab5e7dd682a52b9e2acd2b8200236396
Fabienne <fabienne.gerschwiler@gmail.com>
2022-11-04 12:24:58 +0000

    pushdescription

--- examples/add_user_grant.js
+++ examples/add_user_grant.js
@@ -1,3 +1,11 @@
+/**
+ * Add a usergrant to a new created/registered user
+ *
+ * Flow: External Authentication, Trigger: Post creation
+ *
+ * @param ctx
+ * @param api
+ */
 function addGrant(ctx, api) {
     api.userGrants.push({
         ProjectID: '<the projects resource ID>',
