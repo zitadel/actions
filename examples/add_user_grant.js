d0194d04a427f7954615f02ec126f04d12d7bb22
Fabienne <fabienne.gerschwiler@gmail.com>
2022-11-04 12:23:41 +0000

    add user grant

--- examples/add_user_grant.js
+++ examples/add_user_grant.js
@@ -1,6 +1,6 @@
 function addGrant(ctx, api) {
     api.userGrants.push({
-        ProjectID: '<the projects resource ID you copied above>',
-        Roles: ['<the role key you copied above>']
+        ProjectID: '<the projects resource ID>',
+        Roles: ['<the role key>']
     });
 }
