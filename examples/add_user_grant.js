3f79290e9494a007c126f5ae3d5ff14b09fe16b6
Silvan <silvan.reusser@gmail.com>
2022-12-01 11:01:38 +0100

    fix: correct field names in add user grant example

--- examples/add_user_grant.js
+++ examples/add_user_grant.js
@@ -8,7 +8,7 @@
  */
 function addGrant(ctx, api) {
     api.userGrants.push({
-        ProjectID: '<the projects resource ID>',
-        Roles: ['<the role key>']
+        projectID: '<the projects resource ID>',
+        roles: ['<the role key>']
     });
 }
