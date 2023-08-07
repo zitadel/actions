d8c8fdb3ccc88022a68eed67e3240365eb25941d
sazzad-eu <128996195+sazzad-eu@users.noreply.github.com>
2023-08-07 13:05:33 +0200

    Update examples/make_api_call.js
    Co-authored-by: Livio Spring <livio.a@gmail.com>

--- examples/make_api_call.js
+++ examples/make_api_call.js
@@ -1,3 +1,12 @@
+/**
+ * Call an HTTP endpoint to send / retrieve information.
+ * This example show a simple POST method and logs the retrieved id.
+ *
+ * Flow: External/Internal Authentication and Complement token, Trigger: all
+ *
+ * @param ctx
+ * @param api
+ */
 let http = require('zitadel/http')
 let logger = require("zitadel/log")
 function make_api_call(ctx, api) {
