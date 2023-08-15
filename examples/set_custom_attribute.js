5fd4e8fdac0e42f8941dfa8430867fcc1a716667
Livio Spring <livio.a@gmail.com>
2023-08-15 19:38:30 +0200

    feat: create SAMLResponse custom attribute example (#19)
    * feat: create saml custom attribute example
    
    * fix flow name
    
    * Update set_custom_attribute.js

---
+++ examples/set_custom_attribute.js
@@ -0,0 +1,25 @@
+/**
+ * Add an custom attribute to the SAMLResponse, if it's not already present.
+ * This example will add the resourceOwner as custom attribute `OrgID`.
+ * It additionally shows how get the user's project authorization and add each role in the form of `projectId:role` as a `Roles` attribute.
+ *
+ * Flow: Complement SAMLResponse, Triggers: Pre SAMLResponse creation
+ *
+ * @param ctx
+ * @param api
+ */
+function setCustomAttribute(ctx, api) {
+    const user = ctx.v1.getUser()
+    api.v1.attributes.setCustomAttribute('OrgID', '', user.resourceOwner)
+
+    if (ctx.v1.user.grants == undefined || ctx.v1.user.grants.count == 0) {
+        return;
+    }
+    let roles = [];
+    ctx.v1.user.grants.grants.forEach(grant => {
+        grant.roles.forEach(role => {
+            roles.push(grant.projectId+':'+role)  
+        })
+    })
+    api.v1.attributes.setCustomAttribute('Roles', '', ...roles)
+  }
