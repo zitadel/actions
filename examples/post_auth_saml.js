c0c651a111770ad3ba938bcd22f0f97e8161bd7c
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2023-09-29 09:55:14 +0200

    Update examples/post_auth_saml.js
    Co-authored-by: Livio Spring <livio.a@gmail.com>

--- examples/post_auth_saml.js
+++ examples/post_auth_saml.js
@@ -10,10 +10,26 @@ function map(ctx, api) {
     if (ctx.v1.externalUser.externalIdpId != "your_external_idp") {
         return
     }
-    api.setFirstName(ctx.v1.providerInfo.attributes["urn:oid:2.5.4.42"][0]);
-    api.setLastName(ctx.v1.providerInfo.attributes["urn:oid:2.5.4.4"][0]);
-    api.setEmail(ctx.v1.providerInfo.attributes["urn:oid:1.3.6.1.4.1.5923.1.1.1.6"][0]);
-    api.setEmailVerified(true);
-    api.setDisplayName(ctx.v1.providerInfo.attributes["urn:oid:2.5.4.3"][0]);
-    api.setPreferredUsername(ctx.v1.providerInfo.attributes["urn:oid:0.9.2342.19200300.100.1.1"][0]);
+    // the attribute names below represent the crewjam IDP example, be sure to update them to match your provider info
+    let firstname = ctx.v1.providerInfo.attributes["urn:oid:2.5.4.42"];
+    let lastname = ctx.v1.providerInfo.attributes["urn:oid:2.5.4.4"];
+    let email = ctx.v1.providerInfo.attributes["urn:oid:1.3.6.1.4.1.5923.1.1.1.6"];
+    let displayname = ctx.v1.providerInfo.attributes["urn:oid:2.5.4.3"];
+    let username = ctx.v1.providerInfo.attributes["urn:oid:0.9.2342.19200300.100.1.1"];
+    if (firstname != undefined) {
+    	api.setFirstName(firstname[0]);
+    }
+    if (lastname != undefined) {
+	api.setLastName(lastname[0]);
+    }
+    if (email != undefined) {
+    	api.setEmail(email[0]);
+	api.setEmailVerified(true);
+    }
+    if (displayname != undefined) {
+	api.setDisplayName(displayname[0]);
+    }
+    if (username != undefined) {
+	api.setPreferredUsername(username[0]);
+    }
 }
